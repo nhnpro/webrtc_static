3ef399ead3eb3f6f5834b5a9d1885cd823a6646b
Nguyễn Hoàng Nam <nhnpro.nguyenhoang@gmail.com>
2023-11-24 17:57:40 +0700

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [.iOS(.v12), .macOS(.v10_13), .visionOS(.v1)],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/livekit/webrtc-xcframework-static/releases/download/114.5735.09/WebRTC.xcframework.zip",
+            checksum: "7d86eb6a1d5d757d601a51168b2e7a73e78b7908feca8313644358a397df1772"
+        ),
+    ]
+)
