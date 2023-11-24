ec633f8471fe9f7f5d34f72cd1a1e559fa1da329
Nguyễn Hoàng Nam <nhnpro.nguyenhoang@gmail.com>
2023-11-24 18:29:54 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/livekit/webrtc-xcframework-static/releases/download/114.5735.09/WebRTC.xcframework.zip",
+            url: "https://github.com/nhnpro/webrtc_static/releases/download/114.5735.09/WebRTC.xcframework.zip",
             checksum: "7d86eb6a1d5d757d601a51168b2e7a73e78b7908feca8313644358a397df1772"
         ),
     ]
